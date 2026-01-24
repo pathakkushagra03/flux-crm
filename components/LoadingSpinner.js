2a7da664a9d70e0cac0569b56665eade07e79289
kushagra_ <pathakkushagra03@gmail.com>
2026-01-24 09:54:53 +0000

    Add LoadingSpinner component for loading indication

---
+++ components/LoadingSpinner.js
@@ -0,0 +1,17 @@
+'use client'
+
+export default function LoadingSpinner({ size = 'md' }) {
+  const sizes = {
+    sm: 'w-4 h-4',
+    md: 'w-8 h-8',
+    lg: 'w-12 h-12',
+  }
+
+  return (
+    <div className="flex items-center justify-center">
+      <div
+        className={`${sizes[size]} border-4 border-gray-700 border-t-white rounded-full animate-spin`}
+      />
+    </div>
+  )
+}
