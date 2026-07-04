b5d4425fa6e70e7d112558a33d5f37e1ab6ed7c5
kushagra_ <pathakkushagra03@gmail.com>
2026-07-04 10:03:55 +0000

    Remove env configuration from next.config.js
    Removed environment variables from next.config.js.

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const nextConfig = {
   images: {
     domains: ['dl.airtable.com', 'v5.airtableusercontent.com'],
   },
-  env: {
-    NEXT_PUBLIC_AIRTABLE_API_KEY: process.env.NEXT_PUBLIC_AIRTABLE_API_KEY,
-    NEXT_PUBLIC_AIRTABLE_BASE_ID: process.env.NEXT_PUBLIC_AIRTABLE_BASE_ID,
-  },
 }
 
 module.exports = nextConfig
